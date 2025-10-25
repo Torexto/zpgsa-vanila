f92f2ab68fcfa9a4fc7359804bcd1bcc99b8ddbb
torexto <tomasztrela8@gmail.com>
2025-10-25 20:54:37 +0200

    experimantal refresh on sw update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import Zpgsa from "./zpgsa";
+import {registerSW} from "virtual:pwa-register"
+
+const updateSW = registerSW({
+   immediate: true,
+   onNeedRefresh() {
+      console.log("Nowa wersja dostępna. Odświeżam.");
+      updateSW(); // aktywuje nowego SW
+      window.location.reload(); // reload strony
+   },
+   onOfflineReady() {
+      console.log("Aplikacja gotowa offline");
+   }
+})
 
 await Zpgsa.new("map");
