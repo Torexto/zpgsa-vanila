55567590267f69d2ea16f32156690ece11c4c43d
Torexto <tomasztrela8@gmail.com>
2026-03-13 20:28:09 +0100

    UPDATE

--- vite.config.ts
+++ vite.config.ts
@@ -3,85 +3,63 @@ import {VitePWA} from 'vite-plugin-pwa';
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-   server: {
-      proxy: {
-         "/api/buses": {
-            target: "http://bielawa.trapeze.fi",
-            changeOrigin: true,
-            rewrite: _ =>
-               "/bussit/web?command=olmapvehicles&action=getVehicles"
-         }
-      }
-   },
-   plugins: [
-      vercel(),
-      VitePWA({
-         injectRegister: 'script-defer',
-         registerType: 'autoUpdate',
-         manifest: {
-            name: "Zpgsa",
-            short_name: "Zpgsa",
-            theme_color: "#000000",
-            background_color: "#000000",
-            display: "standalone",
-            scope: "./",
-            start_url: "./",
-            icons: [
-               {
-                  "src": "icons/icon-192x192.png",
-                  "sizes": "192x192",
-                  "type": "image/png",
-                  "purpose": "maskable any"
-               },
-               {
-                  "src": "icons/icon-512x512.png",
-                  "sizes": "512x512",
-                  "type": "image/png",
-                  "purpose": "maskable any"
-               }
-            ]
-         },
-         workbox: {
-            skipWaiting: true,
-            clientsClaim: true,
-            maximumFileSizeToCacheInBytes: 10485760,
+    server: {
+        proxy: {
+            "/api/buses": {
+                target: "http://bielawa.trapeze.fi",
+                changeOrigin: true,
+                rewrite: _ =>
+                    "/bussit/web?command=olmapvehicles&action=getVehicles"
+            }
+        }
+    },
+    vercel: {
+        rewrites: [
+            {
+                "source": "/api/buses",
+                "destination": "http://bielawa.trapeze.fi/bussit/web?command=olmapvehicles&action=getVehicles"
+            }
+        ]
+    },
+    plugins: [
+        vercel(),
+        VitePWA({
+            devOptions: {
+                enabled: true,
+            },
+            injectRegister: 'script-defer',
+            registerType: 'autoUpdate',
+            manifest: {
+                name: "Zpgsa",
+                short_name: "Zpgsa",
+                theme_color: "#000000",
+                background_color: "#000000",
+                display: "standalone",
+                scope: "./",
+                start_url: "./",
+                icons: [
+                    {
+                        "src": "icons/icon-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable any"
+                    },
+                    {
+                        "src": "icons/icon-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable any"
+                    }
+                ]
+            },
+            workbox: {
+                skipWaiting: true,
+                clientsClaim: true,
+                maximumFileSizeToCacheInBytes: 10485760,
 
-            navigateFallback: "/index.html",
+                navigateFallback: "/index.html",
 
-            globIgnores: ['**\/api\/**\/*'],
-            globPatterns: ["**/*.{js,css,ico,png,svg,webmanifest}"],
-
-            runtimeCaching: [
-               {
-                  urlPattern: ({request}) => request.mode === 'navigate',
-                  handler: 'NetworkFirst',
-                  options: {
-                     cacheName: 'html'
-                  }
-               },
-               {
-                  urlPattern: /\.(json)$/,
-                  handler: 'NetworkFirst',
-                  options: {
-                     cacheName: 'json'
-                  }
-               },
-               {
-                  urlPattern: /\.(js|css)$/,
-                  handler: 'NetworkFirst',
-                  options: {
-                     cacheName: 'logic'
-                  }
-               },
-               {
-                  urlPattern: /\.(png|xml|txt|webmanifest)/,
-                  handler: 'CacheFirst',
-                  options: {
-                     cacheName: 'assets'
-                  }
-               }
-            ]
-         }
-      })
-   ]
+            }
+        })
+    ]
 });
